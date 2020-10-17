f8b719fd0dc948055f4b13af42f92dc1f9059655
Ömer Akkuş <52671489+omeristnow@users.noreply.github.com>
2020-10-17 14:19:11 +0300

    Add files via upload

---
+++ bin/main.dart
@@ -0,0 +1,15 @@
+/// final const var and dynamic
+main() {
+  final firstname = 'izzet';
+  final String lastname = 'Haydar';
+  final int yearsold = 44;
+
+  const String hello = 'Hello World ! ';
+  const int years = 2020;
+
+  dynamic value;
+  value = 'Hello World';
+  value = 44;
+  value = false;
+  value = ['hello', 55, false];
+}
